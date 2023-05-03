ac618fb7a9c2d4805c7b0ae772f05eb2c35b73ee
Furkan Nabi Sumji <103491179+furkannabisumji@users.noreply.github.com>
2023-05-03 12:42:02 +0530

    initial

---
+++ hardhat.config.js
@@ -0,0 +1,46 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const { testmnemonic,mnemonic } = require('../web3/secrets.json');
+
+module.exports = {
+  defaultNetwork: "mainnet",
+  networks: {
+    local: {
+      url: "http://127.0.0.1:7545",
+      chainId: 1337,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: testmnemonic}
+    },
+    hardhat: {
+    },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: mnemonic}
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: mnemonic}
+    }
+  },
+  solidity: {
+  version: "0.8.9",
+  settings: {
+    optimizer: {
+      enabled: true
+    }
+   }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+};
