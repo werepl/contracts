327ad01afe6f99d0026a9927f64ba79742c1680b
Furkan Nabi Sumji <103491179+furkannabisumji@users.noreply.github.com>
2024-01-31 11:51:40 +0530

    bug fix

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,7 @@ async function main() {
   const IT = await ethers.getContractFactory("IT");
   const ITContract = await IT.deploy();
   const Reward = await ethers.getContractFactory("Reward");
-  const RewardContract = await Reward.deploy(ethers.BigNumber.from('50000000000000000000000"'));
+  const RewardContract = await Reward.deploy(ethers.BigNumber.from('50000000000000000000000'));
   const Pass = await ethers.getContractFactory("Pass");
   const PassContract = await Pass.deploy("0x2B402F4aec180Fb4188Df7a703d3861f0137855B");
   const Validate = await ethers.getContractFactory("Validate");
